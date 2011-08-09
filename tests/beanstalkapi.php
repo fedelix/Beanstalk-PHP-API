e1f537a763fe7dae00eddd01ee8b9af4101a7935
Chris Barr <chris.barr@ntlworld.com>
2011-08-09 17:16:18 +0100

    More initial wWork on beanstalk test class

--- tests/beanstalkapi.php
+++ tests/beanstalkapi.php
@@ -1,3 +1,24 @@
 <?php
 
+//
+// Perform unit testing on the BeanstalkAPI object using PHPUnit
+//
+
 require(dirname(__FILE__) . "/../lib/beanstalkapi.class.php");
+
+
+class BeanstalkAPITest extends PHPUnit_Framework_TestCase
+{
+	protected $Beanstalk;
+	
+	protected function setUp()
+	{
+		$this->Beanstalk = new BeanstalkAPI();
+	}
+	
+	
+	public function testFindAllRepositories()
+	{
+		
+	}
+}
