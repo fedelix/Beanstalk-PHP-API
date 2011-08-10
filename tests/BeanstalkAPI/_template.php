e16fdf8f6a92df9659f9421228e54f8f359bf202
Chris Barr <chris.barr@ntlworld.com>
2011-08-11 00:14:31 +0100

    Created test template file

---
+++ tests/BeanstalkAPI/_template.php
@@ -0,0 +1,26 @@
+<?php
+
+//
+// Perform unit testing on the TEMPLATE part of the BeanstalkAPI object using PHPUnit
+//
+
+
+class TEMPLATETest extends PHPUnit_Framework_TestCase
+{
+	protected $Beanstalk;
+	
+	/**
+	 * Generic function to check return type from all API calls - can be used as a wrapper
+	 */
+	protected function doReturnTypeCheck($apiResult)
+	{
+		$this->assertEquals('SimpleXMLElement', get_class($apiResult));
+		
+		return $apiResult;
+	}
+	
+	protected function setUp()
+	{
+		$this->Beanstalk = new BeanstalkAPI('account', 'user', 'pass');
+	}
+}
